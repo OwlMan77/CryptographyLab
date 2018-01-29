6709069e8d42dec7061023d3deb5f5f522ee99da
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2018-01-29 03:35:07 +0000

    add html for decode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,43 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  inputMessage: string;
+  inputEncodeMessage: string;
+  inputDecodeMessage: string;
   alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
   ];
   encryptedAlphabet: string[] = [];
-  answerArray = [];
-  cipher: string;
+  answerIndexArray: any[] = [];
+  encodeAnswerArray = [];
+  decodeAnswerArray = [];
+  encodeCipher: string;
+  decodeCipher: string;
   encryptedAnswer: string;
+  decryptedAnswer: string;
 
-  encode() {
-    if (!this.cipher) {
+  keywordEncode() {
+    if (!this.encodeCipher) {
         alert('Please input a cipher');
-    } else if (!this.inputMessage) {
+    } else if (!this.inputEncodeMessage) {
         alert('Please input a message');
     } else {
-        // split our word up into letters
-        const cipherSplit = this.cipher.split('');
 
-        console.log(cipherSplit);
+        // clears previous input
+        this.clear('encode');
+        // split our word up into letters
+        const cipherArray = this.encodeCipher.split('');
 
         // set new alphabet to use cipher
       this.encryptedAlphabet = this.alphabet.filter((letter) => {
-        return cipherSplit.indexOf(letter) < 0;
-      })
-      ;
+        return cipherArray.indexOf(letter) < 0;
+      });
 
-      // send encrypted
-      cipherSplit.forEach((letter) => {
+        // adds our cipher to beginning of array
+      cipherArray.push.apply(cipherArray, this.encryptedAlphabet);
+
+        // spilts our message into an array
+      const messageArray = this.inputEncodeMessage.split('');
+
+        // finds the index of each letter in the alphabet
+      messageArray.forEach((letter) => {
         if (letter !== ' ') {
-          this.answerArray.push(this.encryptedAlphabet.indexOf(letter));
+         this.answerIndexArray.push(this.alphabet.indexOf(letter));
+       } else {
+          this.answerIndexArray.push(' ');
+       }
+      });
+        // pushes each new letter into a final message array
+      this.answerIndexArray.forEach((index) => {
+        if (index !== ' ') {
+          this.encodeAnswerArray.push(cipherArray[index]);
         } else {
-          this.answerArray.push(' ');
+          this.encodeAnswerArray.push(cipherArray[index]);
         }
       });
 
-      // take message and encrypts it
-      this.encryptedAnswer = this.answerArray.join();
+
+      // take message array and joins it into a coded message
+      this.encryptedAnswer = this.encodeAnswerArray.join('');
     }
+  }
+
+
+  clear(params: string) {
+      this.encryptedAlphabet = [];
+      this.answerIndexArray = [];
+    if (params === 'encode') {
+      this.encodeAnswerArray = [];
+      this.encryptedAnswer = null;
+    } else if (params === 'decode') {
+      this.decodeAnswerArray = [];
+      this.decryptedAnswer = null;
+    }
+
   }
 }
